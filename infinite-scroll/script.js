837282ae2833bdcfc2b68f3dc8a8e20f2528bf4d
baryska <55710842+baryska@users.noreply.github.com>
2020-07-08 11:43:41 +0200

    finished

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -2,6 +2,9 @@ console.log('funguju')
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = []
 
 //DRY pro atributy
@@ -12,11 +15,24 @@ function setAttributes(element, attributes) {
 }
 
 //Z dokumentace Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'vGe--UdSmigzT-zcPm4YPnoqthAdXpEJwfK3NP_Eb-c'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function imageLoaded() {
+  console.log('image loaded')
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    loader.hidden = true;
+    ready = true
+    console.log('ready = ', ready)
+  }
+}
+
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length
+  console.log('total images', totalImages)
   //pro každou fotku
   photosArray.forEach((photo) => {
     const item = document.createElement('a')
@@ -35,6 +51,9 @@ function displayPhotos() {
     // img.setAttribute('src', photo.urls.regular);
     // img.setAttribute('alt', photo.alt_description)
     // img.setAttribute('title', photo.alt_description)
+
+    img.addEventListener('load', imageLoaded)
+
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -52,5 +71,13 @@ async function getPhotos() {
 
   }
 }
+
+//Pokud uživatel zascrolluje pod určitou hranici (vše co je na stránce (i neviditelné) - 1000px), zavolá se funkce getPhotos()
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false
+    getPhotos()
+  }
+})
 
 getPhotos()
